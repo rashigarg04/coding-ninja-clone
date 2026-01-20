e41705db7323f39c7d3e062fe5fa9432dc354639
Rashi Garg <146509998+rashigarg04@users.noreply.github.com>
2026-01-20 22:27:28 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+﻿// App.jsx
+import React from "react";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import CoursesGrid from "./components/CoursesGrid";
+import PracticeGrid from "./components/PracticeGrid";
+import EventsGrid from "./components/EventsGrid";
+import CampusGrid from "./components/CampusGrid";
+import Footer from "./components/Footer";
+
+
+export default function App() {
+    return (
+        <div className="bg-gray-50 min-h-screen">
+            <Navbar />
+            <HeroSection />
+            <CoursesGrid />
+            <PracticeGrid />
+            <EventsGrid />
+            <CampusGrid />
+            <Footer />
+        </div>
+    );
+}
